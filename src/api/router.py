e6678da8c2643d8664234a7ac8fc747dec5d19a8
dcx <861059472@qq.com>
2025-09-18 17:59:33 +0800

    chrome_ext

--- src/api/router.py
+++ src/api/router.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from .config.router import router as config_router
-from .short_url.router import router as short_url_router
 
 router = APIRouter(tags=["api"],prefix="/api")
 
-#router.include_router(config_router,prefix="/config")
-router.include_router(short_url_router,prefix="/short_url")
+router.include_router(config_router,prefix="/config")
