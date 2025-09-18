3c3f9d9d736ba76c1618b97d45eb999bd8fbae46
dcx <861059472@qq.com>
2025-09-18 16:18:45 +0800

    short url

--- src/api/router.py
+++ src/api/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .config.router import router as config_router
+from .short_url.router import router as short_url_router
 
-router = APIRouter(tags=["config"],prefix="/config")
+router = APIRouter(tags=["api"],prefix="/api")
 
-router.include_router(config_router)
+#router.include_router(config_router,prefix="/config")
+router.include_router(short_url_router,prefix="/short_url")
