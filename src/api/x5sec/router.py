7a2d7776e28f4663f3d92d47593aa89c9af7d11d
dcx <861059472@qq.com>
2025-09-20 13:39:29 +0800

    dcx

--- src/api/x5sec/router.py
+++ src/api/x5sec/router.py
@@ -16,5 +16,5 @@ async def current(
     cookie_dict = parse_cookie_str(params.cookie_str)
     if params.cookies:
         cookie_dict.update(params.cookies)
-    x5sec = get_x5sec(params.slide_url, params.ua, cookie_dict, params.proxies, params.body, **params)
+    x5sec = get_x5sec(params.slide_url, params.ua, cookie_dict, params.proxies, params.body)
     return x5sec
