b1c86e0008bf95ff623962217fd2d3c402dea66f
dcx <861059472@qq.com>
2025-09-20 18:41:45 +0800

    damai_cookie

--- src/api/x5sec/config.py
+++ src/api/x5sec/config.py
@@ -16,5 +16,5 @@ class APIInfo(object):
     headers = {
         "Content-Type": "application/json"
     }
-    timeout = 12
+    timeout = 16
     ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
