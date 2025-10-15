4314be4b3b4a3eb3dd8141c009f981dfbc4c375d
dcx <dcx@192.168.10.2>
2025-10-15 23:03:25 +0800

    dcx

--- src/api/chrome_ext/test.py
+++ src/api/chrome_ext/test.py
@@ -10,9 +10,10 @@ url_redis_key = "chrome_ext:item_url"
 import time
 with open("src/tmp/20251015_item_url", "r") as f:
     item_id_list = f.read().splitlines()
-    item_id_list = [item_id.split(" ")[0] for item_id in item_id_list][100:]
-    #item_id_dict = {item_id.split(" ")[0]:item_id.split(" ")[1] for item_id in item_id_list}
-    #redis_pool.hmset(url_redis_key, item_id_dict)
+    #item_id_list = [item_id.split(" ")[0] for item_id in item_id_list][100:]
+    item_id_dict = {item_id.split(" ")[0]:item_id.split(" ")[1] for item_id in item_id_list}
+    redis_pool.hmset(url_redis_key, item_id_dict)
+time.sleep(10000)
 i = 0
 r_type = sys.argv[1]
 for j in range(200):
